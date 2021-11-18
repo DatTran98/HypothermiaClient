3399aa0672085f80ed0338bff4bcbab6a59d2ae2
datviboi@gmail.com <datviboi@gmail.com>
2021-11-18 23:30:54 +0700

    No1 - Fix export no data

--- Temp/app/src/main/java/com/hust/temp/sevicesretrofit/DataChangeListener.java
+++
@@ -1,9 +0,0 @@
-package com.hust.temp.sevicesretrofit;
-
-import com.hust.temp.entities.StudentInfo;
-
-import java.util.List;
-
-public interface DataChangeListener {
-    void listenDataChange(List<StudentInfo> account);
-}
